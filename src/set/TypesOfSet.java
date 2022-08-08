b44ba295c2158caab4ad4dae6351ac611ec96ed9
Rafael de Luca <deluca1712@gmail.com>
2022-08-08 14:11:35 -0300

    types of set

---
+++ src/set/TypesOfSet.java
@@ -0,0 +1,29 @@
+package set;
+
+public class TypesOfSet {
+
+	public static void main(String[] args) {
+		System.out.println("1. Universal Set - a set that contains everything."
+				+ "Mathematical notaion is a capital U\n"
+				+ "2. Empty set: set that contains nothing. Zero elements\n"
+				+ "\t\tMathematical notaion is a Null set\n"
+				+ "\t\tEmpty set: {x E N | 4 < x 5 }\n"
+				+ "3. Singleton set: set that has only one element\n"
+				+ "\t\tA{3}\n"
+				+ "\t\tA = {{}} is a singleton set."
+				+ "4. Finite set: finite numbers of elements\n"
+				+ "\t\tA={1,5,7,12}\n"
+				+ "5. Infinite set: set that hava infinet elements\n"
+				+ "\t\tA={1,5,7,12}\n"
+				+ "6. Subset: something that is smaller than a set.\n"
+				+ "\nCARDINAL NUMBER OF A SET: number os distinct elements of a site\n"
+				+ "A = {2,6,7,4,4,5,5}\n"
+				+ "n(A)=5 distinct elements\n"
+				+ "\nEQUIVALENT SET\n"
+				+ "set ar equivalent if they have the same Cardinality.\n"
+				+ "Equivalent is different of equal!\n"
+				+ "The mathematical element of equivalent is a ~");
+
+	}
+
+}
