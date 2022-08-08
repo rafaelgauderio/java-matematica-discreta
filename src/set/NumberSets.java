00e104a3e5dd4fecf48f04c12b61c640a7dbe918
Rafael de Luca <deluca1712@gmail.com>
2022-08-08 12:59:53 -0300

    set Numbers

---
+++ src/set/NumberSets.java
@@ -0,0 +1,25 @@
+package set;
+
+public class NumberSets {
+
+	public static void main(String[] args) {
+				
+		System.out.println("Natural Number: 0,1,2,3...\n"
+				+ "Represent by a capital N\n"
+				+ "Integers Numbers: ...,-2.-1,0,1,2,...\n"
+				+ "Represent by a capital Z\n"
+				+ "Racional Numbers: 4/2, 10/9, 1/4\n"
+				+ "Can be represented as a fraction os two integers\n"
+				+ "Represent by a capital Q\n"
+				+ "Irrational Numbers: sqrt(3), phi, euller,\n"
+				+ "Represent by a capital Q~\n"
+				+ "Real numbers: irrational + rational\n"
+				+ "Represent by capital R\n"
+				+ "Imaginary Numbers:i\n"
+				+ "square of negative numbers, represent by a capital I\n"
+				+ "Complex Numbers: 2 + 3i \n"
+				+ "Combination of Real numbers and imaginary numbers\n"
+				+ "Represent by a Capital C");
+	}
+
+}
