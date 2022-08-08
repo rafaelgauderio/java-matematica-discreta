5b3b9285a5772a42f4f714f175495a739f649b6d
Rafael de Luca <deluca1712@gmail.com>
2022-08-08 13:27:45 -0300

    set Builder Notation

---
+++ src/set/SetBuilderNotation.java
@@ -0,0 +1,17 @@
+package set;
+
+public class SetBuilderNotation {
+
+	public static void main(String[] args) {		
+		System.out.println("Set-Buiter Notation\n"
+				+ "The set with all the shapes, which the shares are red. Consider red equal y\n"
+				+ "Mathematical notation\n"
+				+ "{x | x = y}\n"
+				+ "Set with all the elementes that are greater than six\n"
+				+ "{x | z > 6}\n"
+				+ "{x E R | -3 < x < 10}\n"
+				+ "The set of all x's that are Real Numbers, such that x is greater than or equal to 0\n"
+				+ "{x E R | x >= 0}");
+	}
+
+}
