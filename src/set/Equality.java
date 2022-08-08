70001d586d6e0f66543a6bb7c1c1492750b0aee8
Rafael de Luca <deluca1712@gmail.com>
2022-08-08 13:10:44 -0300

    Equality of sets

---
+++ src/set/Equality.java
@@ -0,0 +1,18 @@
+package set;
+
+public class Equality {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("Axiom of Extension\n"
+				+ "A set is determined by whats its elementes are not the order in which they be listed or the fact\n"
+				+ "that some elements might be listed more than onde.\n"
+				+ "Nether repetition nor order matters in sets\n"
+				+ "Não importa a ordem dos elementos e nem se existem elementos repetidos\n"
+				+ "A = {1,4,5,1,5,5,5,7}\n"
+				+ "B={7,1,4,5}\n"
+				+ "Podem ser considerados iguais os sets acima. Mesmo havendo repetições e ordem diferentes, pois contém os mesmos elementos.");
+		
+	}
+
+}
