a6131edc14c3e72fe0cf49826217b7d9879f3434
Rafael de Luca <deluca1712@gmail.com>
2022-08-09 17:48:03 -0300

    PowerSet

---
+++ src/set/PowerSet.java
@@ -0,0 +1,17 @@
+package set;
+
+public class PowerSet {
+
+	public static void main(String[] args) {
+
+		System.out.println("\nPower set of A, denoted P(A), is the set of all subsets of A.\n"
+				+ "Example\n"
+				+ "A = {a, d, f}\n"
+				+ "P(A) = {}, {a},{d},{f}, {a,d}, {a,f},{d,f}, {a,d,f}\n"
+				+ "X = {1, 7}\n"
+				+ "P(X)= {},{1},{7},{1,7}\n"
+				+ "the elements (n) of P(A) are pow(2,n)\n");
+
+	}
+
+}
