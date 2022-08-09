a515bb9c6cf069d5bbba5b4cda69c8111c4be233
Rafael de Luca <deluca1712@gmail.com>
2022-08-09 17:41:12 -0300

    Subset

---
+++ src/set/Subset.java
@@ -0,0 +1,23 @@
+package set;
+
+public class Subset {
+
+	public static void main(String[] args) {
+		
+		System.out.println("If A and B are sets, then A is called a subset of B IF, and ONLY IF,\n"
+				+ "every element of A is also an Element of B.\n"
+				+ "A B\n"
+				+ "Every set is a subset of itself.\n"
+				+ "The empty set is s subset of every set.\n"
+				+ "\nPROPER SUBSET\n"
+				+ "A = {1,3,4}\n"
+				+ "B = {1,2,3,4,5}\n"
+				+ "A is proper subset of B, because B have at least one element different of A.\n"
+				+ "{2} is a subset of {{1}, 2, {3}}\n"
+				+ "{2}  is not a subset of {1,{2},{3}}\n"
+				+ "You have to see the diference between a element of a set and a subset.");
+				
+
+	}
+
+}
