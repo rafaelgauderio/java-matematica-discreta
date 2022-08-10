c613e95a4b6d1af90d110c1d889441bc3fc437f9
Rafael de Luca <deluca1712@gmail.com>
2022-08-10 12:16:41 -0300

    Cartesian Plane

---
+++ src/set/CartesianProduct.java
@@ -0,0 +1,20 @@
+package set;
+
+public class CartesianProduct {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gives set A and B, the Cartesian Product of A and B, denoted A x B\n"
+				+ "\tand read A cros B, is the set of all ordered pairs(a,b)\n"
+				+ "\twhere a is in A and b is in B.\n"
+				+ "A x B = {(a,b) | a E A and b E B}\n"
+				+ "A = {3,4} B = {5,6}\n"
+				+ "A x B = (3,5),(3,6),(4,5),(4,6)\n"
+				+ "A x B is different of B x A\n"
+				+ "A x B x C= {(a,b,c) | a E A , b E B and c E C}\n"
+				+ "(A x B x C) is different of (A x B) x C\n"
+				+ "A(4 elements) x B(5 elements) then A X B have 4 x 5 = 20 elements");
+
+	}
+
+}
