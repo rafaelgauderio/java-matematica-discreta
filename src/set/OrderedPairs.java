fa8fa337e187971bd8229d221cb58b648859c4f8
Rafael de Luca <deluca1712@gmail.com>
2022-08-10 11:57:52 -0300

    Orderes Pairs == ordered n-tuples

---
+++ src/set/OrderedPairs.java
@@ -0,0 +1,17 @@
+package set;
+
+public class OrderedPairs {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\t\tOrdered Pairs\n"
+				+ "They are an ordering of objects such  that the order of the objects matter.\n"
+				+ "(1,2,4,5) are not equal (1,2,5,4)\n"
+				+ "Orderes n-Tuples: the order of the elements matter.\n"
+				+ "(x1,x2,x3,...,xn) = (y1,y2,y3,..yn)\n"
+				+ "x1 = y1\n"
+				+ "(r,x) = order pair is the same as a ordered 2-tuple\n"
+				+ "(a,b,c) = ordered 3-tuple\n" );		
+	}
+
+}
