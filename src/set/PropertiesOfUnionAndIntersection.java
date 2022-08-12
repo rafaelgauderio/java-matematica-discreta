95ce193568cd40acdf0176b67367f346020d1bc6
Rafael de Luca <deluca1712@gmail.com>
2022-08-12 16:37:11 -0300

    Properties of Unian an Intersection

---
+++ src/set/PropertiesOfUnionAndIntersection.java
@@ -0,0 +1,20 @@
+package set;
+
+public class PropertiesOfUnionAndIntersection {
+
+	public static void main(String[] args) {
+		System.out.println("\n1. Commutative Law\n"
+				+ "A U B = B U A , A In B = B In A\n"
+				+ "\n2. Associative Law : How the sets are grouped does not matter.\n"
+				+ "(A U B) U C = A U (B U C)\n"
+				+ "\n3. Distribitive Law\n"
+				+ "A U (B In C) = (A U B) In (A U C)\n"
+				+ "\n4. Law\n"
+				+ "A U {} = A, A In {} = {}\n"
+				+ "\n5. Law\n"
+				+ "A U Un = Un\n"
+				+ "\n6.Idempotent Law\n"
+				+ "A U A = A, A In A = A");
+	}
+
+}
