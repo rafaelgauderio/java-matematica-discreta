ac242fa80bf5c38e505fc66a28399f578f6804f7
Rafael de Luca <deluca1712@gmail.com>
2022-08-12 16:52:25 -0300

    Difference and Complement

---
+++ src/set/SetOperationsDifferenceComplement.java
@@ -0,0 +1,21 @@
+package set;
+
+public class SetOperationsDifferenceComplement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nDifference\n"
+				+ "A - B = All the elements that are in A, but not in B.\n"
+				+ "All the elements that are only in A\n"
+				+ "A = {1,4,5,6} , B = {5,6,9}\n"
+				+ "A - B = {1,4}\n"
+				+ "B - A = {9}\n"
+				+ "\nComplement\n"
+				+ "U = {3,4,5,6,7,8}\n"
+				+ "A = {2,3,4}\n"
+				+ "Ac = All the elements that are in U that are not in A.\n"
+				+ "Ac = {5,6,7,8}");
+
+	}
+
+}
