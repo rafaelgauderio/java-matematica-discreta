6af088e7c5300d53493c669a85986169bf8c3f6e
Rafael De Luca <deluca1712@gmail.com>
2022-08-13 19:41:16 -0300

    partitions of a set

---
+++ src/set/Partition.java
@@ -0,0 +1,21 @@
+package set;
+
+public class Partition {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Disjoints: if que sets have no elementes in commom\n"
+				+ "A int B = empty"
+				+ "Example A = {2,3,5} , B = {a,c 9,}\n"
+				+ "A and B are Disjoint because they have no commom element.\n"
+				+ "\nMaturelly disjoint set\n"
+				+ "A lot of sets such none of them have any element in commom.\n"
+				+ "A good analogy is to compare the set to islands, that are isolated to each other.\n"
+				+ "A partition is one of these islands.\nAll these islands together form the set A.\n"
+				+ "\n A limite or infinite collection of none empty sets {A1, A2,...} is a partition of a set A. If and only if:\n"
+				+ "\t1. A is the Union of all the sets.\n"
+				+ "\t2. The sets ar maturelly disjoint.\n");
+
+	}
+
+}
