db40716235bd887d7fdffd14d3008aa85014ec21
Rafael De Luca <deluca1712@gmail.com>
2022-08-13 16:51:03 -0300

    Properperties of Differente and Complement

---
+++ src/set/PropertiesOfDifferenceComplement.java
@@ -0,0 +1,17 @@
+package set;
+
+public class PropertiesOfDifferenceComplement {
+
+	public static void main(String[] args) {
+		
+		System.out.println("A U Ac = U\n"
+				+ "(Ac)c = A\n"
+				+ "Uc = {}\n"
+				+ "{}c = U\n"
+				+ "A - B = A int Bc\n"
+				+ "Example\n"
+				+ "If A = {1, 2, 3}, and B is {4, 5}, then A - B` is:\nn" + 
+				"The difference of the set A with B` (which is A) is the empty set.\n");
+	}
+
+}
