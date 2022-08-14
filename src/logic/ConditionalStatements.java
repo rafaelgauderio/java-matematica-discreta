2a9e5b4b317579fbef0d9fb5c4c96cb0fd228f3b
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 17:57:19 -0300

    Conditional Statements - Vera Fisher é Falsa

---
+++ src/logic/ConditionalStatements.java
@@ -0,0 +1,24 @@
+package logic;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Conditional Statements\n"
+				+ "\tif...then\n"
+				+ "\tif you work hard then you get paid.\n"
+				+ "\tp -> q\n"
+				+ "\tp = hypothesis\n"
+				+ "\tq = conclusion\n"
+				+ "truth table\n"
+				+ "p | q | p -> q\n"
+				+ "T | T | T\n"
+				+ "T | F | T\n"
+				+ "F | T | F\n"
+				+ "F | F | T\n"
+				+ "\nif the hypothesis is true and you do not get the conclusion, then you have a broken promisse.\n"
+				+ "Mnemônico = Vera Fisher é Falsa\n");
+		
+	}
+
+}
--- src/logic/DeMorgansLaws.java
+++ src/logic/DeMorgansLaws.java
@@ -5,7 +5,10 @@ public class DeMorgansLaws {
 	public static void main(String[] args) {
 		System.out.println("De Morgan Laws\n"
 				+ "\t~(p ^ q ) == ~p v ~q\n"
-				+ "\t~(p v q) == ~p ^ ~q\n");
+				+ "\t~(p v q) == ~p ^ ~q\n"
+				+ "Example:\n"
+				+ "0 < x <= 3 == x > 0 and x<=3\n"
+				+ "~( x > 0 and x <=3 ==  x <=0 or x >3 ");
 	}
 
 }
