d05a21f1935180d98f0e5491c1c245c25be7f69f
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 16:29:47 -0300

    Tautologie, contradiction and contingency

---
+++ src/logic/TautologiesContradictions.java
@@ -0,0 +1,18 @@
+package logic;
+
+public class TautologiesContradictions {
+
+	public static void main(String[] args) {
+		
+		System.out.println("TAUTOLOGIE\n"
+				+ "\tTautologie is a statement from that is always true, regardless of the values\n"
+				+ "\tof individual statements.\n"
+				+ "\nCONTRADICTION\n"
+				+ "\tThe opposite of tautologies.\n"
+				+ "\tThe statement is always false, regardless of the values of individual statements."
+				+ "Just construct the truth table and see if is is always true ou always false.\n"
+				+ "\nCONTINGENCY\n"
+				+ "\tSome values of the truth table are true and some are false.\n");
+	}
+
+}
