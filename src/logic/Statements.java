40ee17518adb08f7957a7f41815aa7adb7db8117
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 12:45:07 -0300

    logic - Statements

---
+++ src/logic/Statements.java
@@ -0,0 +1,12 @@
+package logic;
+
+public class Statements {
+
+	public static void main(String[] args) {
+		System.out.println("Statements\n"
+				+ "\tA statement (or proposition) is a sentence that is either true or false(but never both\n"
+				+ "\tA statement can not take two values.\n");
+
+	}
+
+}
