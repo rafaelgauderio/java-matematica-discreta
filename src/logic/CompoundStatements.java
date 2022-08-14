1d709f1c319d294012a48b54555d9763bdbc1caa
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 13:05:38 -0300

    Compound Statements

---
+++ src/logic/CompoundStatements.java
@@ -0,0 +1,26 @@
+package logic;
+
+public class CompoundStatements {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Compound Statements\n"
+				+ "\tWhen you represent more than omde statement in the sentence.\n"
+				+ "Example:\n"
+				+ "\tConecting statements using LOGICAL CONNECTORS.\n"
+				+ "\tp = I am tall\n"
+				+ "\tq = John is my name\n"
+				+ "\t~p or q\n"
+				+ "\tI am not tall or John is my name.\n"
+				+ "CONJUNCTION: p ^ q\n"
+				+ "\tbut equals and, neither nor equals and\n"
+				+ "DISJUNCTION: p or q\n"
+				+ "NEGATION: ~q\n"
+				+ "Example:\n"
+				+ "a <= x <= 15\n"
+				+ "x>=a and x <=15\n"
+				+ "x < = 20\n"
+				+ "x = 20 or x <20");
+	}
+
+}
--- src/logic/Statements.java
+++ src/logic/Statements.java
@@ -5,7 +5,12 @@ public class Statements {
 	public static void main(String[] args) {
 		System.out.println("Statements\n"
 				+ "\tA statement (or proposition) is a sentence that is either true or false(but never both\n"
-				+ "\tA statement can not take two values.\n");
+				+ "\tA statement can not take two values.\n"
+				+ "Example\n"
+				+ "3 -x = 12 is not a statements\n"
+				+ "Depending on tha value of x, the sentence can be true or false. Right now it is neither.\n"
+				+ "3+5 =20\n"
+				+ "This is a statement because is is true or false, but not both. Is the case it is false.");
 
 	}
 
