9c5a060ddbc5b0e69b0f3baa74e736e5341d26f3
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 15:57:13 -0300

    truth table example

--- src/logic/TruthTables.java
+++ src/logic/TruthTables.java
@@ -5,20 +5,29 @@ public class TruthTables {
 	public static void main(String[] args) {
 		System.out.println("AND\n"
 				+ "p q | p ^ q\n"
-				+ "\nT T | T\n"
-				+ "\nF T | F\n"
-				+ "\nT F | F\n"
-				+ "\nF F | F\n"
+				+ "T T | T\n"
+				+ "F T | F\n"
+				+ "T F | F\n"
+				+ "F F | F\n"
 				+ "\nOR\n"
-				+ "Just one need to be True for the compound statement to be tru\n"
+				+ "Just one need to be True for the compound statement to be true\n"
 				+ "OR is consider inclusive\n"
-				+ "\nOR OR\n"
+				+ "p q | p or q\n"
+				+ "T T = T\n"
+				+ "T F = T\n"
+				+ "F T = T\n"
+				+ "F F = F\n"				
+				+ "\nXOR = OR OR\n"
 				+ "exclusive\n"
 				+ "p q | or p or q\n"
 				+ "T T = F\n"
 				+ "F T = T\n"
 				+ "T F = T\n"
-				+ "F F = F\n"); 
+				+ "F F = F\n"
+				+ "\nNEGATION\n"
+				+ "p ~p\n"
+				+ "T F\n"
+				+ "F T\n"); 
 		
 	}
 
