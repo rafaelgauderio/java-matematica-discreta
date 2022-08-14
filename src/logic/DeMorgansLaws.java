c991e2b427692befa652bd67f2d77d9fc78bd543
Rafael De Luca <deluca1712@gmail.com>
2022-08-14 17:01:30 -0300

    De Morgan Laws

---
+++ src/logic/DeMorgansLaws.java
@@ -0,0 +1,11 @@
+package logic;
+
+public class DeMorgansLaws {
+
+	public static void main(String[] args) {
+		System.out.println("De Morgan Laws\n"
+				+ "\t~(p ^ q ) == ~p v ~q\n"
+				+ "\t~(p v q) == ~p ^ ~q\n");
+	}
+
+}
