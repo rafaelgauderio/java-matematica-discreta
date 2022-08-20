6bf64b4613ebe981bdb552131c6dbbaffb3d6970
Rafael De Luca <deluca1712@gmail.com>
2022-08-20 19:33:21 -0300

    Biconditional Statements

---
+++ src/logic/Biconditional.java
@@ -0,0 +1,25 @@
+package logic;
+
+public class Biconditional {
+
+	public static void main(String[] args) {
+		
+		System.out.println("BICONDITIONAL STATEMENTS\n"
+				+ "The sequence of the actions to go both ways.\n"
+				+ "Seria o se e somente se\n"
+				+ "IF AND ONLY IF (iff).\n"
+				+ "p <-> q\n"
+				+ "if p happens the q happens AND if q happens then p\n"
+				+ "Truth table\n"
+				+ "If The propositions have the same value the biconditional is True.\n"
+				+ "p | q | p <-> q\n"
+				+ "T | T | T\n"
+				+ "T | F | F\n"
+				+ "F | T | F\n"
+				+ "F | F | T\n"
+				+ "q <-> q == (p -> q)^(q -> p)\n"
+				+ "Biconditional if the oposite of the XOR.\n");
+
+	}
+
+}
--- src/logic/ConverseAndInverse.java
+++ src/logic/ConverseAndInverse.java
@@ -10,12 +10,14 @@ public class ConverseAndInverse {
 				+ "The hypothesis becomes the conclusion and the conclusion becomes the hypothesis.\n"
 				+ "\tq -> q\n"
 				+ "INVERSE\n"
-				+ "Negate the prepositions, but do not change their positions.\n"
+				+ "Negate the propositions, but do not change their positions.\n"
 				+ "\t~p -> ~q\n"
 				+ "Example:\n"
 				+ "IF it rains THEN the party is canceled.\n"
 				+ "CONVERSE: IF the party is canceled THEN it rains.\n"
-				+ "INVERSE: IT it NOT rains THEN the party is NOT canceled.\n");
+				+ "INVERSE: IT it NOT rains THEN the party is NOT canceled.\n"
+				+ "The converse and the inverse are LOGICALLY EQUIVALENT\\n"
+				+ "A conditional statement and its converse are NOT logically equivalent");
 	}
 
 }
