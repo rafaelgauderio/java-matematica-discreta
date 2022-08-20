25c0472cb1edf30d709d80603fd38d623b9fb155
Rafael De Luca <deluca1712@gmail.com>
2022-08-20 19:15:10 -0300

    Converse and Inverse the conditional

---
+++ src/logic/ConverseAndInverse.java
@@ -0,0 +1,21 @@
+package logic;
+
+public class ConverseAndInverse {
+
+	public static void main(String[] args) {
+		
+		System.out.println("PROPOSITION\n"
+				+ "\tp -> q\n"
+				+ "CONVERSE\n"
+				+ "The hypothesis becomes the conclusion and the conclusion becomes the hypothesis.\n"
+				+ "\tq -> q\n"
+				+ "INVERSE\n"
+				+ "Negate the prepositions, but do not change their positions.\n"
+				+ "\t~p -> ~q\n"
+				+ "Example:\n"
+				+ "IF it rains THEN the party is canceled.\n"
+				+ "CONVERSE: IF the party is canceled THEN it rains.\n"
+				+ "INVERSE: IT it NOT rains THEN the party is NOT canceled.\n");
+	}
+
+}
