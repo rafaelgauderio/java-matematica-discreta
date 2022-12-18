d1039ff770be261dc79b91219c1d876f568a3c35
Rafael De Luca <deluca1712@gmail.com>
2022-12-18 20:42:09 -0300

    Black Boxes and Gates

---
+++ src/logic/BlackBoxesAndGates.java
@@ -0,0 +1,21 @@
+package logic;
+
+public class BlackBoxesAndGates {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nBLACK BOXES"
+				+ "\nThink about the inputs and the outputs, but not in the set of circuits inside the black box."
+				+ "\nAnalogous to a printer. You do not know what happens insedi the printer, Just know the input and the print output."
+				+ "\nYou do not know what algorithms is goind inside the printer."
+				+ "\n\nGATES"
+				+ "\nGates are simple circuits"
+				+ "\nType of gates: not, and, or"
+				+ "\n0 = off = open = F / 1 = on = closed = T"
+				+ "\n\tNOT gate: is the negation. 2 input and 1 output. 1 input gets a 0 input."
+				+ "\n\tAND gate: is the AND truth table. 2 inputs and 1 output"
+				+ "\n\tOR gate: is the OR truth table. 2 inputs and 1 output"
+				+ "\nTOOL to construct circuits online: LOGIC.LY ");
+	}
+
+}
