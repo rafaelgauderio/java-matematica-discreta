1dbefa9845f9910a3b748bbfebfa2be818103a3b
Rafael De Luca <deluca1712@gmail.com>
2022-12-18 21:19:14 -0300

    Boolean expressions

---
+++ src/logic/BooleanExpressions.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class BooleanExpressions {
+	
+	public static void main (String [] args) {
+		
+		System.out.println("\nBollean Expressions"
+				+ "\nBoolean variable: can take one of two values. True or False. Never both."
+				+ "\nBoolean expressions have just boolean variables and connectives (OR, NOT, AND, XOR)");
+		
+	}
+
+}
