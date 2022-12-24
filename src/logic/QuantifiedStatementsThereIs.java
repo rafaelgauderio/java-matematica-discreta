4539e8a5f010996eb7ba90cd2b65250d13a27c67
Rafael De Luca <deluca1712@gmail.com>
2022-12-24 18:21:09 -0300

    Quantified Statements - THERE IS

---
+++ src/logic/QuantifiedStatementsThereIs.java
@@ -0,0 +1,15 @@
+package logic;
+
+public class QuantifiedStatementsThereIs {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Quantified Statement - THERE IS"
+				+ "\n\tExistential quantifier. The symbom is a E mirrored"
+				+ "\n\tThere is means that exist AT LEAST ONE element such the statement is true."
+				+ "\n\t∃ birds b| ∀ time t, b sings."
+				+ "\n\ttSome birds sing all the time.");
+
+	}
+
+}
