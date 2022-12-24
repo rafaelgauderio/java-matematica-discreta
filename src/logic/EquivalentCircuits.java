bd6dc8ec60e24fa0a8fff64979216ffae089a92e
Rafael De Luca <deluca1712@gmail.com>
2022-12-24 11:05:14 -0300

    Equivalent Circuits

---
+++ src/logic/EquivalentCircuits.java
@@ -0,0 +1,13 @@
+package logic;
+
+public class EquivalentCircuits {
+
+	public static void main(String[] args) {
+		
+		System.out.println("EQUIVALENT CIRCUITS"
+				+ "\n\tLogical Circuits are EQUIVALENT if and only if, their inputs and the outputs are the same."
+				+ "\n\tThese can be use to represent complex circuits by simple circutir that are equivalent.");
+
+	}
+
+}
--- src/logic/TruthTableAndCircuits.java
+++ src/logic/TruthTableAndCircuits.java
@@ -6,6 +6,8 @@ public class TruthTableAndCircuits {
 		
 		System.out.println("\nHow to turn a truth table into a circuit."
 				+ "\nUsing only logical connective."
+				+ "\nFirst thing is to identify all those outputs that are one."
+				+ "\nSecond step is to create a logical expression that will give you one such site that give yout true."
 				+ "\n\tinput T F F, output T. It´s necessary to negate q and r."
 				+ "\n\tp ^ ~q ^ ~r == T ^ T ^ T = T"
 				+ "\n\tAfter using AND, thEN join the expression with OR connective"
