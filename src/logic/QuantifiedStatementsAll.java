430bdeda1c4ef4877521e758c5002d585e963e27
Rafael De Luca <deluca1712@gmail.com>
2022-12-24 17:54:15 -0300

    Quantified statements - ALL

---
+++ src/logic/QuantifiedStatementsAll.java
@@ -0,0 +1,20 @@
+package logic;
+
+public class QuantifiedStatementsAll {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nQuantified Statements - All"
+				+ "\n\tStatements is a declaration that is true or false. Never both."
+				+ "\n\tWe use quantifiers to quantify a statement."
+				+ "\n\tAll (universal quantifier) - the symbol is a A upside down (∀)"
+				+ "\n\tUniversal statement envolves all the elements of a set."
+				+ "\n\tCounter Example is a example that show that the universal statement is NOT true."
+				+ "\n\t∀ x ∈ R, if x > 3 then x^2 > 9"
+				+ "\n\tFor all the real numbers, if any real number is greater than 3, then its square is greater than 9"
+				+ "\n\tA = {-40,-20,-10,1,15,20,29,40,50}"
+				+ "\n\t∀ x ∈ R, if x is odd then x > 0");
+
+	}
+
+}
