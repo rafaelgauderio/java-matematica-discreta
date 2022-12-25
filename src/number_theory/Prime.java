0bafbcd0853156a164838e7ae2f5193240e1e33d
Rafael De Luca <deluca1712@gmail.com>
2022-12-25 19:48:26 -0300

    Number theory - Prime Numbers

---
+++ src/number_theory/Prime.java
@@ -0,0 +1,17 @@
+package number_theory;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Prime Numbers"
+				+ "\n\tAll those integers that are greater than one and the only positive divisors that they have is themselves and 1."
+				+ "\n\tExampe: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43..."
+				+ "\n\tThe only even Prime number is 2."
+				+ "\nThe fundamental theorem of Arithmetics"
+				+ "\n\tAny integer that is greater than 1, it is either a prime number or created by the multiplication of prime numbers."
+				+ "\n\tEvery integer that is greater than 1, it is ehter a prime number or a composite number.");
+
+	}
+
+}
