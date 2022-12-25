f797faa09e4416b5eb7a433de38350923011ec55
Rafael De Luca <deluca1712@gmail.com>
2022-12-25 19:16:25 -0300

    Number theory - Parity

---
+++ src/number_theory/Parity.java
@@ -0,0 +1,29 @@
+package number_theory;
+
+public class Parity {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nParity"
+				+ "\n\tParity means whether an integer is even or odd"
+				+ "\n\tEven: 2k rule. Multiply 2 for a number to obtain the event number."
+				+ "\n\tThe variable k has to a integer."
+				+ "\n\tZero is a even number. Zero is algo a integer."
+				+ "\n\tExample: 2 * 3 = 6. Six is a even number."
+				+ "\n\tOdd number: 2k + 1 rule."
+				+ "\n\tExample: 2 * 4 + 1 = 11. Eleven is a odd number"
+				+ "\n\tThe rules 2k and 2k + 1 apply to negative numbers."
+				+ "\n\tEvery integer are even or odd."
+				+ "\nRules to add and subtraction"
+				+ "\n\teven + event = even"
+				+ "\n\teven + odd = odd"
+				+ "\n\todd + odd = even = 2(2k+1 + 2k+1)"
+				+ "\n\todd + even = odd"
+				+ "\nRules to multiplication"
+				+ "\n\todd * odd = odd"
+				+ "\n\todd * even == even * odd = even"
+				+ "\n\teven * even = even");
+
+	}
+
+}
