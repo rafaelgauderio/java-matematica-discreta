193c8223fcee5cd003dc8a0e9c42cd5b1eec14cb
Rafael De Luca <deluca1712@gmail.com>
2022-12-27 18:51:50 -0300

    Number Theory - Greatest common diviser

---
+++ src/number_theory/GCD.java
@@ -0,0 +1,15 @@
+package number_theory;
+
+public class GCD {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nGCD - Greatest common divisor"
+				+ "\n\tIs the greatest common divisor between two or more numbers."
+				+ "\n\tIn the list of commom divisor, find the greatest number or using Prime Factorization."
+				+ "\n\tExample: gcd(6,10) = 2. 6 is divisible by 1,2,3,6. 10 is divisible by 1,2,5,10. GCD between 6 and 10 are 2."
+				+ "\n\tExample: gcd (24,16) = 8. 24 is divisible by 1,2,3,4,6,8,12. 16 is divisible by 1,2,4,8,16");
+
+	}
+
+}
