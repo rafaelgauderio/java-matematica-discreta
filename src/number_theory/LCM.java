124b5cc2c6456bc147ae3fffe5c8b324dda06f7d
Rafael De Luca <deluca1712@gmail.com>
2022-12-27 19:25:19 -0300

    Number Theory - Least Common Multiple

---
+++ src/number_theory/LCM.java
@@ -0,0 +1,17 @@
+package number_theory;
+
+public class LCM {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nLeast Commom Multiple"
+				+ "\n\tSmallest commom multiple between two or numbers or using Prime factorization"
+				+ "\n\tlcm(10,4) = 20."
+				+ "\n\tMultiples of for: 4, 8, 12, 16, 20, 24... Multiples of 10: 10, 20, 30, 40, 50..."
+				+ "\n\tlcm(3,9,21) = 3 * 3 * 7 = 63"
+				+ "\n\tUsing prime factorization: 3 = 3. 9 = 3 * 3. 21 = 3 * 7. I used the wicht list the three has occured the most. 3 occured 2 times in 9 prime factorization."
+				+ "\n\tMathematical formula: LCM (x,y) = | x * y | / GCD(x,y)");
+
+	}
+
+}
