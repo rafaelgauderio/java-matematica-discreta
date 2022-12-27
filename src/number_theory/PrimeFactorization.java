c55791470751efd7773aae2f10a5c4bb456705b3
Rafael De Luca <deluca1712@gmail.com>
2022-12-27 18:40:51 -0300

    Number Theory - Prime Factorization

---
+++ src/number_theory/PrimeFactorization.java
@@ -0,0 +1,22 @@
+package number_theory;
+
+public class PrimeFactorization {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\nFactors"
+				+ "\n\tFactors are the numbers that you multiply to get another number."
+				+ "\n\tExample: 2 * 4 = 8, 2 and 4 are factors of eigth."
+				+ "\nPrime Factorization"
+				+ "\n\tIs basically finding those prime numbers so that if you multiply them, we get the original number."
+				+ "\n\tExample: 2 * 2 * 5 = 20. 2, 2, and 5 are the prime numbers that multiplied give you 20."
+				+ "\n\tExample: 147 = 3 * 7 * 7"
+				+ "\n\tThe process is to divide by the smallest prime number possible and keep doing this until you have one as result."
+				+ "\n\tAnalogous to least common multiple, but only whith prime numbers."
+				+ "\nUnique Prime Factorization theorem"
+				+ "\n\tThere is only ane possible set of prime numbers that can create another number."
+				+ "\n\tThere is a unique prime factorization for each number");
+
+	}
+
+}
